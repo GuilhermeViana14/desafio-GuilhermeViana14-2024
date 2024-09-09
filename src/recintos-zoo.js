68777acb12c195c73d9037d26a91c55e09ca24fb
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2024-09-09 01:38:15 -0300

    qeicionado 2 construtores

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,5 +1,33 @@
 class RecintosZoo {
 
+    constructor(){
+
+        this.recintos = [
+            {numero: 1, bioma:'savana', tamanho: 10, animais: [{especie: 'MACACO', quantidade: 3, tamanho: 1}] },
+            {numero: 2, bioma:'floresta', tamanho: 5, animais: [] },
+            {numero: 3, bioma:'savana e rio', tamanho: 7, animais: [{especie: 'GAZELA', quantidade: 1, tamanho: 2}] },
+            {numero: 4, bioma:'rio', tamanho: 8, animais: [] },
+            {numero: 5, bioma:'savana', tamanho: 9, animais: [{especie: 'LEAO', quantidade: 1, tamanho: 3}] },
+
+
+
+
+         ]
+    
+        this.animaisPermitidos = {
+            'LEAO': { tamanho: 3, biomas: ['savana'] },
+            'LEOPARDO': { tamanho: 2, biomas: ['savana'] },
+            'CROCODILO': { tamanho: 3, biomas: ['rio'] },
+            'MACACO': { tamanho: 1, biomas: ['savana', 'floresta'] },
+            'GAZELA': { tamanho: 2, biomas: ['savana'] },
+            'HIPOPOTAMO': { tamanho: 4, biomas: ['savana', 'rio'] },
+        };
+        
+    }
+    
+    
+    
+    
     analisaRecintos(animal, quantidade) {
     }
 
